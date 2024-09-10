81072f24b29c7f79407af2e6263d3eb2b1a35abc
LukynkaCZE <jiraklukas058@seznam.cz>
2024-09-10 15:58:49 +0200

    oops forgot build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,10 +16,11 @@ repositories {
 
 dependencies {
     implementation("cz.lukynka:pretty-log:1.4")
-    implementation("cz.lukynka:lkws:1.1")
+    implementation("cz.lukynka:lkws:1.2")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.0")
+    implementation("net.lingala.zip4j:zip4j:2.11.5")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
